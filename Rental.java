7903264bec5b5fd50f83fba9b5ce067b4a16fd41
nilswendland <65414607+nilswendland@users.noreply.github.com>
2021-05-09 18:50:55 +0200

    Add files via upload
    First commit

---
+++ Rental.java
@@ -0,0 +1,22 @@
+package FowlerRefactorExcersize;
+
+public class Rental {
+
+    private Movie movie;
+    private int daysRented;
+    public Rental(Movie newmovie, int newdaysRented) {
+        movie = newmovie;
+        daysRented = newdaysRented;
+    }
+    public int getDaysRented() {
+        return daysRented;
+    }
+  
+	
+	public Movie getMovie() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
